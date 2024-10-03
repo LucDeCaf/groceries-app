65b87181679cf2a39fee080c409b1e7c7cfefb71
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-10-03 16:47:04 +0200

    add navbar to bottom

--- frontend-react-native/src/App.tsx
+++ frontend-react-native/src/App.tsx
@@ -1,11 +1,18 @@
 import '@azure/core-asynciterator-polyfill';
 import 'react-native-url-polyfill/auto';
 import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, View, Text } from 'react-native';
+import {
+    ActivityIndicator,
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+} from 'react-native';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { Session } from '@supabase/supabase-js';
 import { supabase } from '@/lib/supabase';
 import { Auth } from '@/components/Auth';
+import { Button, Tab, TabView } from '@rneui/themed';
 
 export default function App() {
     const [loading, setLoading] = useState(true);
@@ -42,5 +49,43 @@ interface PageProps {
 }
 
 function Page({ session, setSession }: PageProps) {
-    return <View></View>;
+    const [tabIndex, setTabIndex] = useState(0);
+
+    return (
+        <View style={styles.page}>
+            <ScrollView style={styles.contentContainer}>
+                <Button>
+                    <Text style={styles.buttonText}>Add an item</Text>
+                </Button>
+            </ScrollView>
+            <View style={styles.footer}>
+                <Tab value={tabIndex} onChange={setTabIndex} variant='primary'>
+                    <Tab.Item title='List' />
+                    <Tab.Item title='List' />
+                    <Tab.Item title='List' />
+                </Tab>
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+    },
+    contentContainer: {
+        flex: 1,
+        padding: 16,
+        paddingTop: 32,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 24,
+    },
+    footer: {
+        backgroundColor: '#fff',
+        shadowColor: 'black',
+        shadowRadius: 10,
+        shadowOpacity: 0.1,
+    },
+});
