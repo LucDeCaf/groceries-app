f486a9879326406f05618152d9da756bf9b98a2b
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-11-26 11:48:16 +0200

    add shadcn

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
@@ -7,6 +8,11 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     // Don't optimize these packages as they contain web workers and WASM files.
     // https://github.com/vitejs/vite/issues/11672#issuecomment-1415820673
