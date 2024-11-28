b667f13a4fad1a408b370cc01e4be3a91b6f8b09
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2024-11-28 10:15:20 +0200

    add button variants + fix classes being overwritten

--- frontend-react/src/components/Button.tsx
+++ frontend-react/src/components/Button.tsx
@@ -1,7 +1,8 @@
 import { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: 'primary' | 'secondary' | 'danger';
+  variant: 'primary' | 'secondary' | 'link' | 'danger';
 }
 
 export const Button = (props: ButtonProps) => {
@@ -9,22 +10,41 @@ export const Button = (props: ButtonProps) => {
     case 'primary':
       return (
         <button
-          className='rounded-md bg-blue-500 text-white text-md p-1'
           {...props}
+          className={twMerge(
+            'rounded-md border-2 border-blue-500 bg-blue-500 text-white text-md p-2',
+            props.className,
+          )}
         ></button>
       );
     case 'secondary':
       return (
         <button
-          className='rounded-md border-2 border-blue-500 text-blue-500 bg-white text-md p-1'
           {...props}
+          className={twMerge(
+            'rounded-md border-2 border-blue-500 text-blue-500 bg-white text-md p-2',
+            props.className,
+          )}
+        ></button>
+      );
+    case 'link':
+      return (
+        <button
+          {...props}
+          className={twMerge(
+            'underline text-blue-500 text-md p-0',
+            props.className,
+          )}
         ></button>
       );
     case 'danger':
       return (
         <button
-          className='rounded-md bg-red-500 text-white text-md p-1'
           {...props}
+          className={twMerge(
+            'rounded-md bg-red-500 border-2 border-red-500 text-white text-md p-2',
+            props.className,
+          )}
         ></button>
       );
   }
